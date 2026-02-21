a393933604e211a5665b64337f558d3d4eaac878
AdithyaSuresh1453 <sureshadithya7288@gmail.com>
2026-02-21 16:00:50 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -14,12 +14,6 @@ createRoot(root).render(
     <App />
   </StrictMode>
 );
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
